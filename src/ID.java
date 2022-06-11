97c4c290e176ceb13a98f39db51368e289f6e3d8
Alexandr Nazyan <alexander.nazyan@yandex.ru>
2022-06-11 12:01:02 +0300

    изменил модификатор доступа, изменил return

--- src/ID.java
+++ src/ID.java
@@ -3,9 +3,7 @@ import java.util.Objects;
 public class ID {
     private int id = 0;
 
-    protected int generator() {
-        id += 1;
-        int newId = id;
-        return newId;
+    public int generator() {
+        return id = id +=1;
     }
 }
